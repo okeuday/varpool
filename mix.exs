eb17e64cd738bf093835c0fe1aae38e0ac6ff900
Michael Truog <mjtruog@gmail.com>
2015-12-23 13:22:05 -0800

    Version 1.5.1.

---
+++ mix.exs
@@ -0,0 +1,26 @@
+defmodule Varpool.Mixfile do
+  use Mix.Project
+
+  def project do
+    [app: :varpool,
+     version: "1.5.1",
+     description: description,
+     package: package,
+     deps: deps]
+  end
+
+  defp deps do
+    []
+  end
+
+  defp description do
+    "Erlang Process Pools as a Local Variable"
+  end
+
+  defp package do
+    [files: ~w(src doc test rebar.config README.markdown),
+     maintainers: ["Michael Truog"],
+     licenses: ["BSD"],
+     links: %{"GitHub" => "https://github.com/okeuday/varpool"}]
+   end
+end
