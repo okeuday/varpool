ff4a1131d522a0fce8e21dfdb366db72c0d80abf
Michael Truog <mjtruog@gmail.com>
2017-03-28 19:13:34 -0700

    Version 1.7.0.

--- mix.exs
+++ mix.exs
@@ -1,9 +1,12 @@
+#-*-Mode:elixir;coding:utf-8;tab-width:2;c-basic-offset:2;indent-tabs-mode:()-*-
+# ex: set ft=elixir fenc=utf-8 sts=2 ts=2 sw=2 et nomod:
+
 defmodule Varpool.Mixfile do
   use Mix.Project
 
   def project do
     [app: :varpool,
-     version: "1.6.0",
+     version: "1.7.0",
      description: description(),
      package: package(),
      deps: deps()]
@@ -15,7 +18,7 @@ defmodule Varpool.Mixfile do
   end
 
   defp deps do
-    [{:reltool_util, "~> 1.6.0"}]
+    [{:reltool_util, "~> 1.7.0"}]
   end
 
   defp description do
